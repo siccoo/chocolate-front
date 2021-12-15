bbabe4299a26535a699c82240d3d11af6a1e9c44
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-15 20:57:51 +0100

    updates on Header components and assets folder

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import ChocoLogo from "../../assets/choco-logo.png";
+import { searchIcon } from "../../assets";
 
-const index = () => {
-    return (
-        <header>
-            Hello Header
-        </header>
-    )
-}
+const index = (props) => {
+  return (
+    <header className="header flex justify-sb">
+      <div className="logo">
+        <img src={ChocoLogo} alt="logo" style={{ width: "40px" }} />
+      </div>
+      <div>
+        <img src={searchIcon} alt="Search" />
+      </div>
+    </header>
+  );
+};
 
-export default index
+export default index;
