62f5fd2bff3bf5ab478c3b4853bd18fcaabd065c
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-15 21:36:48 +0100

    updates on Tabs component

---
+++ Exercise.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// Write a simple and efficient Javascript program that returns the smallest non-occurring
+// integer in a given Array.
+// E.g: Given an Array1 = [1, 3, 6, 4, 1, 2] returns 5, and Array2 = [5, -1, -3] returns 1.
+
+const A = [-1, -4];
+function solution(A) {
+    let max = A.reduce((acc, val) => acc > val ? acc : val);
+    let missing = 1;
+    for (let i = max + 1; i > 0; i--) {
+        found = A.find(el => el == i);
+        if(found === undefined) {
+            missing = i;
+        }
+    }
+    return missing; 
+}
+console.log(solution(A));
