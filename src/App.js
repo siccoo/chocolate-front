757be040dccab2b94efebedf6c613f567f0dc033
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-15 21:12:26 +0100

    updates on SearchInput component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import SearchInput from './components/SearchInput';
 
 function App() {
   return (
     <div className="App m-20">
       <Header/>
+      <h2 className="mtb-20 app-quote">Find the best music of Chocolate studio</h2>
+      <SearchInput />
     </div>
   );
 }
---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,13 @@
+import { searchIconGrey } from "../../assets";
+import "./style.css";
+
+const SearchInput = (props) => {
+  return (
+    <div className="search-input-container flex">
+      <img src={searchIconGrey} alt="search" />
+      <input placeholder="Find your music" />
+    </div>
+  );
+};
+
+export default SearchInput;
