3019e3f48b84708446cb30545caf5d46a9b8e631
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-16 11:02:43 +0100

    updates in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Header from './components/Header';
 import SearchInput from './components/SearchInput';
 import Tabs from "./components/Tabs";
 import AudioList from "./components/AudioList";
+import FixFooter from "./components/FixFooter";
+
+import { baseUrl } from "./config";
 
 import './App.css';
 
 function App() {
   const [list, setList] = useState(false);
+  const [appData, setAppData] = useState({});
+  const [audioList, setAudioList] = useState([]);
+  const [trackIndex, setTrackIndex] = useState(-1);
 
   const onBackButtonPress = () => {
     setList(false);
   };
 
+  const onItemSelect = (tab, type) => {
+    if (tab in appData) {
+      if (type in appData[tab]) {
+        const audioList = appData[tab][type];
+        setAudioList(audioList);
+      } else {
+        //to remove old items from array
+        setAudioList([]);
+      }
+    } else {
+      //to remove old items from array
+      setAudioList([]);
+    }
+
+    setList(true);
+  };
+
+  const onTrackSelect = (index) => {
+    setTrackIndex(index);
+  };
+
+  useEffect(() => {
+    fetch(`${baseUrl}/song`)
+      .then((res) => res.json())
+      .then((jsonResp) => {
+        // console.log({ jsonResp });
+        setAppData(jsonResp.appData);
+      })
+      .catch((error) => {
+        // console.log({ error });
+      });
+  }, []);
+
   return (
     <div className="App m-20">
       <Header/>
-      <h2 className="mtb-20 app-quote">Find the best music of Chocolate studio</h2>
+      <h2 className="mtb-20 app-quote">Find your best music of Chocolate city studio</h2>
       <SearchInput />
-      <Tabs />
-      {list && (<AudioList onBackButtonPress={onBackButtonPress} />)}
+      <Tabs onItemSelect={onItemSelect} tabData={appData["homeScreen"]} />
+      {list && (<AudioList audioList={audioList}
+          onTrackSelect={onTrackSelect} onBackButtonPress={onBackButtonPress} />)}
+
+      <FixFooter trackIndex={trackIndex} audioList={audioList} />
     </div>
   );
 }
