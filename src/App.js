071b5d664cba3f56f835602f1c317f94a7eefd70
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-16 04:31:40 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
+import {useState} from "react";
 import Header from './components/Header';
 import SearchInput from './components/SearchInput';
 import Tabs from "./components/Tabs";
-import AudioList from "./components/AudioList"
+import AudioList from "./components/AudioList";
+
+import './App.css';
 
 function App() {
+  const [list, setList] = useState(false);
+
+  const onBackButtonPress = () => {
+    setList(false);
+  };
+
   return (
     <div className="App m-20">
       <Header/>
       <h2 className="mtb-20 app-quote">Find the best music of Chocolate studio</h2>
       <SearchInput />
+      <Tabs />
+      {list && (<AudioList onBackButtonPress={onBackButtonPress} />)}
     </div>
   );
 }
